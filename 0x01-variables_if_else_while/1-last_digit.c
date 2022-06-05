18b72d96ba941235b2ee2cfa3d9fc5be81ab86ee
fosuhemaa <abenapenteng@gmail.com>
2022-06-05 15:36:16 +0000

    Prints last digit in assigned variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 1 = n % 10;
 if (1 > 5)
 {
-printf("Last digit of %d and is greater than 5\n", n, 1);
+printf("Last digit of % d and is greater than 5\n", n, 1");
 }
 else if (1 == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, 1");
+printf("Last digit of % d is % d and is 0\n", n, 1");
 }
 else
 {
-printf("Last digit of % d is % d and is less than 6 and not 0\n",
+printf("Last digit of % d is % d and is less than 6 and not 0\n", n, 1");
 }
 return (0);
 
