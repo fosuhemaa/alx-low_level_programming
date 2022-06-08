a8145f90c8b303bcee5db61520491b5bb4ce6393
fosuhemaa <abenapenteng@gmail.com>
2022-06-08 08:39:47 +0000

    Checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+
+/**
+ * _islower - Shows if input is in lowercase
+ *
+ * 0
+ *
+ * @c: Character in ASCII code
+ *
+ * Return: 1 for lowercase and 0 for other.
+ */
+
+int_islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
