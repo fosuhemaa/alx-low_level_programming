356ac17ad388d39e2d86713b969c642460299813
fosuhemaa <abenapenteng@gmail.com>
2022-06-08 09:09:34 +0000

    Checks alphabet case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+
+/**
+ * _islower - Checks for alphabet case
+ * or shows 0
+ * @c: character in ascii code
+ *
+ * Return:  1 for lowercase aand 0 for other
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
