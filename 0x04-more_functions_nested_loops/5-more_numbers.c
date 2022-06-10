792266311c2b53a3a96a2d177f5b0f1d7c7ed64d
fosuhemaa <abenapenteng@gmail.com>
2022-06-10 00:53:43 +0000

    Draws a straight line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * followed by a new line
- */
-
+ *  * more_numbers - func that prints 10 times the numbers, from 0 to 14
+ *   *
+ *    */
 void more_numbers(void)
 {
-	int i, j;
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 15; j++)
-		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
-		}
-		_putchar('\n');
-	}
+	int i, ch;
+
+		for (i = 0; i < 10; i++)
+			{
+				for (ch = 0; ch < 15; ch++)
+					{
+															if (ch >= 10)
+	 																	_putchar((ch / 10) + 48);
+																		_putchar((ch % 10) + 48);
+																				}
+			_putchar('\n');
+			}
 }
