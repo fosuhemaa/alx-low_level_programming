c72c166229e6b4afe1b28415859e3e955d3a9c6d
fosuhemaa <abenapenteng@gmail.com>
2022-06-13 23:18:42 +0000

    Prints other char of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of two in a string.
+ * @str: input string
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
+
+
